84ae0c795d69b143c3dcc5a68ba85531988bfc5b
Eric Murr <eric.murr@me.com>
2021-03-02 16:35:21 -0600

    chore: remove period in description text

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          I do code.
+          I do code
         </p>
 
         {/* <div className={styles.grid}>
