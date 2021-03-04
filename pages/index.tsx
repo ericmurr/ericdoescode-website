97cde020724e9136001a06b1e9294597bdb3fa64
Eric Murr <eric.murr@me.com>
2021-03-04 00:08:14 -0600

    chore: added and setup prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,29 +2,27 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Eric Does Code</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Eric Does Code</title>
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Hi! I'm <span className={styles.authorName}>Eric</span>
-        </h1>
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    Hi! I'm <span className={styles.authorName}>Eric</span>
+                </h1>
 
-        <p className={styles.description}>
-          I do code
-        </p>
+                <p className={styles.description}>I do code</p>
 
-        {/* <div className={styles.grid}>
+                {/* <div className={styles.grid}>
           <a href="https://www.ericmurr.com" className={styles.card}>
             <h3>My Website &rarr;</h3>
             <p>www.ericmurr.com</p>
           </a>
         </div> */}
-      </main>
-    </div>
-  )
+            </main>
+        </div>
+    );
 }
